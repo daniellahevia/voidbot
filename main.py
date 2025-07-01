1c242af5ac6c31811c502fbf3ee9157ce4114cd0
daniellahevia <sheviaedu@gmail.com>
2025-07-01 19:16:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 
 Get token from environment variable,
-TOKEN = os.getenv("DISCORD_TOKEN")  # Changed to more specific name
+TOKEN = os.getenv("TOKEN")  # Changed to more specific name
 
 intents = discord.Intents.default()
 intents.message_content = True
